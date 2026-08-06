364cb896ebc23939291e783acfd6023a2782b31b
Jerdon07 <jdlitaoen@gmail.com>
2026-08-06 20:25:28 +0800

    Refactor: Update project section titles and improve layout structure

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge"
 import { Card, CardAction, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { projects } from "@/lib/projects"
 import { ArrowRight } from "lucide-react"
@@ -9,16 +10,19 @@ export default function Projects() {
     return (
         <div className="flex flex-col justify-center items-center px-4">
             <div className="my-5 flex-0 text-center w-full md:w-2/3 space-y-4">
-                <h1 className="text-2xl font-bold">My Capstone Project</h1>
-                <h3 className="text-zinc-400 leading-relaxed">An enterprise-level project for my last year in the university.</h3>
+                <h1 className="text-2xl font-bold">My Personal and Team Projects</h1>
+                <h3 className="text-zinc-400 leading-relaxed">A showcase of my personal and collaborative work.</h3>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 overflow-hidden flex-1 w-full md:w-[80%]">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 overflow-hidden flex-1 w-full md:w-[80%] items-stretch">
                 {projects.map((project) => (
-                    <Link href={`/projects/${project.id}`} key={project.id}>
-                        <Card className="pt-0 cursor-pointer border">
+                    <Link href={`/projects/${project.id}`} key={project.id} className="block h-full">
+                        <Card className="pt-0 cursor-pointer border h-full">
                             <div className="relative aspect-video bg-black/35">
                                 <Image src={project.thumbnail} alt={project.title} fill />
+                                <Badge className="absolute right-2 bottom-2 bg-card px-2 py-1 border-card">
+                                    { project.badge }
+                                </Badge>
                             </div>
 
                             <CardHeader>
